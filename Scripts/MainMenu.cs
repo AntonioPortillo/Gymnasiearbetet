4f5ddd48f7826284fa8db23e4835ccda8bfe9fdd
Lilpapi <50100823+AntonioPortillo@users.noreply.github.com>
2020-02-26 11:20:21 +0100

    Update MainMenu.cs

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -5,10 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
- 
+    public GameObject FadeInPanel;
+    public GameObject FadeOutPanel;
+    public float FadeWait;
+
+    private void Awake()
+    {
+        if (FadeInPanel != null)
+        {
+            GameObject panel = Instantiate(FadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
+    
+    public IEnumerator FadeCo()
+    {
+        if (FadeOutPanel != null)
+        {
+            Instantiate(FadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(FadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("SampleScene");
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
     public void NewGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        StartCoroutine(FadeCo());
     }
 
     public void QuitToDesktop()
