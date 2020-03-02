2c4f083f8d400740d1abad251296e8dcd6220db0
Lilpapi <50100823+AntonioPortillo@users.noreply.github.com>
2020-03-02 08:45:54 +0100

    Update PauseManager.cs

--- Scripts/PauseManager.cs
+++ Scripts/PauseManager.cs
@@ -8,20 +8,21 @@ public class PauseManager : MonoBehaviour
     private bool isPaused;
     public GameObject pausePanel;
     public string mainMenu;
-    void Start()
+  
+  void Start()
     {
-        isPaused = false;
+        isPaused = false; // När spelet startas är boolean = false gällande om pauseskärmen är igång eller inte.
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Pause"))
+        if(Input.GetButtonDown("Pause")) // i unity har jag fixat en Axis som innehåller vad som ska ske när man trycker på respektive knapp. På Axis kan man fixa så att spelet dessutom är spelbart på Xbox eller Playstation
         {
             ChangePause(); 
         }
     }
-    public void ChangePause()
+    public void ChangePause() //kopplas till den ena knappen som stänger av pausemenyn för att komma tillbaks till spelet.
     {
         isPaused = !isPaused;
         if (isPaused)
@@ -35,7 +36,7 @@ public class PauseManager : MonoBehaviour
             Time.timeScale = 1f;
         }
     }
-   public void QuitToMain()
+   public void QuitToMain() //kopplas till en knapp som tar en till mainMenu scenen.
     {
         SceneManager.LoadScene(mainMenu);
         Time.timeScale = 1f;
